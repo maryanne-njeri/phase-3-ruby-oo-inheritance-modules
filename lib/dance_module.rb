07548851d9749f50b81c33776911618791dcc401
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-08-29 05:05:48 +0300

    Code along

---
+++ lib/dance_module.rb
@@ -0,0 +1,18 @@
+module dance 
+
+    def twirl
+        "I'm twirling!"
+    end
+
+    def jump 
+        "Look how high am jumping!"
+    end
+
+    def pirouette
+        "I'm doing pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+end
